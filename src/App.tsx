1780fb245506eee353ec80ed2269a6f301688bab
sebranly <sebranly.pinf@gmail.com>
2021-05-03 23:19:43 +0200

    feat: push cards

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,42 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { isMobile } from "react-device-detect";
 
+import TenClubs from "./img/cards/10clubs.svg";
+import AceClubs from "./img/cards/aceclubs.svg";
+import KingClubs from "./img/cards/kingclubs.svg";
+import QueenClubs from "./img/cards/queenclubs.svg";
+import JackClubs from "./img/cards/jackclubs.svg";
+import NineClubs from "./img/cards/9clubs.svg";
+import EightClubs from "./img/cards/8clubs.svg";
+import SevenClubs from "./img/cards/7clubs.svg";
+
+import TenDiamonds from "./img/cards/10diamonds.svg";
+import AceDiamonds from "./img/cards/acediamonds.svg";
+import KingDiamonds from "./img/cards/kingdiamonds.svg";
+import QueenDiamonds from "./img/cards/queendiamonds.svg";
+import JackDiamonds from "./img/cards/jackdiamonds.svg";
+import NineDiamonds from "./img/cards/9diamonds.svg";
+import EightDiamonds from "./img/cards/8diamonds.svg";
+import SevenDiamonds from "./img/cards/7diamonds.svg";
+
+import TenHearts from "./img/cards/10hearts.svg";
+import AceHearts from "./img/cards/acehearts.svg";
+import KingHearts from "./img/cards/kinghearts.svg";
+import QueenHearts from "./img/cards/queenhearts.svg";
+import JackHearts from "./img/cards/jackhearts.svg";
+import NineHearts from "./img/cards/9hearts.svg";
+import EightHearts from "./img/cards/8hearts.svg";
+import SevenHearts from "./img/cards/7hearts.svg";
+
+import TenSpades from "./img/cards/10spades.svg";
+import AceSpades from "./img/cards/acespades.svg";
+import KingSpades from "./img/cards/kingspades.svg";
+import QueenSpades from "./img/cards/queenspades.svg";
+import JackSpades from "./img/cards/jackspades.svg";
+import NineSpades from "./img/cards/9spades.svg";
+import EightSpades from "./img/cards/8spades.svg";
+import SevenSpades from "./img/cards/7spades.svg";
+
 const App = () => {
   if (isMobile) {
     return (
@@ -42,7 +78,73 @@ const App = () => {
         <title>Manille Demo</title>
         <link rel="canonical" href="https://sebranly.github.io/manille-demo" />
       </Helmet>
-      <div>Bonsoir encore</div>
+      <div className="main">
+        <div>Bonsoir encore</div>
+        <div className="container">
+          <div className="flex-one">
+            <div className="table"></div>
+          </div>
+          <div className="flex-one demo-cards">
+            <div className="demo-cards">
+              <img className="demo-card" src={TenClubs} alt="React Logo" />
+              <img className="demo-card" src={AceClubs} alt="React Logo" />
+              <img className="demo-card" src={KingClubs} alt="React Logo" />
+              <img className="demo-card" src={QueenClubs} alt="React Logo" />
+              <img className="demo-card" src={JackClubs} alt="React Logo" />
+              <img className="demo-card" src={NineClubs} alt="React Logo" />
+              <img className="demo-card" src={EightClubs} alt="React Logo" />
+              <img className="demo-card" src={SevenClubs} alt="React Logo" />
+            </div>
+            <div className="demo-cards">
+              <img className="demo-card" src={TenDiamonds} alt="React Logo" />
+              <img
+                className="demo-card demo-card-player-one"
+                src={AceDiamonds}
+                alt="React Logo"
+              />
+              <img
+                className="demo-card demo-card-player-two demo-card-player-one"
+                src={KingDiamonds}
+                alt="React Logo"
+              />
+              <img
+                className="demo-card"
+                src={QueenDiamonds}
+                alt="React Logo"
+                onClick={() => console.log("oui")}
+              />
+              <img className="demo-card" src={JackDiamonds} alt="React Logo" />
+              <img
+                className="demo-card demo-card-used"
+                src={NineDiamonds}
+                alt="React Logo"
+              />
+              <img className="demo-card" src={EightDiamonds} alt="React Logo" />
+              <img className="demo-card" src={SevenDiamonds} alt="React Logo" />
+            </div>
+            <div className="demo-cards">
+              <img className="demo-card" src={TenSpades} alt="React Logo" />
+              <img className="demo-card" src={AceSpades} alt="React Logo" />
+              <img className="demo-card" src={KingSpades} alt="React Logo" />
+              <img className="demo-card" src={QueenSpades} alt="React Logo" />
+              <img className="demo-card" src={JackSpades} alt="React Logo" />
+              <img className="demo-card" src={NineSpades} alt="React Logo" />
+              <img className="demo-card" src={EightSpades} alt="React Logo" />
+              <img className="demo-card" src={SevenSpades} alt="React Logo" />
+            </div>
+            <div className="demo-cards">
+              <img className="demo-card" src={TenHearts} alt="React Logo" />
+              <img className="demo-card" src={AceHearts} alt="React Logo" />
+              <img className="demo-card" src={KingHearts} alt="React Logo" />
+              <img className="demo-card" src={QueenHearts} alt="React Logo" />
+              <img className="demo-card" src={JackHearts} alt="React Logo" />
+              <img className="demo-card" src={NineHearts} alt="React Logo" />
+              <img className="demo-card" src={EightHearts} alt="React Logo" />
+              <img className="demo-card" src={SevenHearts} alt="React Logo" />
+            </div>
+          </div>
+        </div>
+      </div>
     </HelmetProvider>
   );
 };
