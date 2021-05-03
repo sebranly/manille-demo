810a42d945d1ce6d260db5cc51f1aba797240c21
sebranly <sebranly.pinf@gmail.com>
2021-05-03 21:52:30 +0200

    feat: initialize the website

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import { IoIosConstruct } from "react-icons/io";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { isMobile } from "react-device-detect";
+
+const App = () => {
+  if (isMobile) {
+    return (
+      <HelmetProvider>
+        <Helmet>
+          <title>Manille Demo</title>
+          <link
+            rel="canonical"
+            href="https://sebranly.github.io/manille-demo"
+          />
+        </Helmet>
+        <div className="mobile">
+          <h1>Manille Demo</h1>
+          <div>
+            We are working hard to make this website accessible on mobile. In
+            the meantime, please visit it on a computer instead. Thank you for
+            your understanding!
+          </div>
+        </div>
+        <br />
+        <IoIosConstruct className="icon-mobile" size="100px" />
+      </HelmetProvider>
+    );
+  }
+
+  return (
+    <HelmetProvider>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+        data-cross-origin="anonymous"
+      />
+      <Helmet>
+        <title>Manille Demo</title>
+        <link rel="canonical" href="https://sebranly.github.io/manille-demo" />
+      </Helmet>
+      <div>Bonjour</div>
+    </HelmetProvider>
+  );
+};
+
+export default App;
