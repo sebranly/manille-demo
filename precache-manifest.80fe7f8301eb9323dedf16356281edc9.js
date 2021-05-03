3161eb32cd8c8cd7de886a9ac507acdd3292783f
sebranly <sebranly.pinf@gmail.com>
2021-05-03 21:58:31 +0200

    Updates

---
+++ precache-manifest.80fe7f8301eb9323dedf16356281edc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e51676c5c4d9c3e3faf31490b621dd58",
+    "url": "/manille-demo/index.html"
+  },
+  {
+    "revision": "6d8b9a7f8938bf5694b3",
+    "url": "/manille-demo/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "6bebeea02b203607c2c0",
+    "url": "/manille-demo/static/css/main.f85e4518.chunk.css"
+  },
+  {
+    "revision": "6d8b9a7f8938bf5694b3",
+    "url": "/manille-demo/static/js/2.0d65321a.chunk.js"
+  },
+  {
+    "revision": "35ce96af1918caec30eb09d747864582",
+    "url": "/manille-demo/static/js/2.0d65321a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bebeea02b203607c2c0",
+    "url": "/manille-demo/static/js/main.1692ac4d.chunk.js"
+  },
+  {
+    "revision": "50e3a3071fcf1603afca",
+    "url": "/manille-demo/static/js/runtime-main.2bb4be0e.js"
+  }
+]);
