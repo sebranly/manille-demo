20b248fffc2a8619736af2b1136567338c2967fc
sebranly <sebranly.pinf@gmail.com>
2021-05-05 21:46:49 +0200

    chore: run pretty

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
 
-test("renders text", () => {
+test('renders text', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Bonsoir/i);
   expect(linkElement).toBeInTheDocument();
